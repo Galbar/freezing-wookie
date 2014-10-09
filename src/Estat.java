4c367922b43997d5a1612a0aa5a7d535ed149169
Alessio Linares <alessio@alessio.cc>
2014-10-09 15:29:41 +0200

    Second Initial commit

---
+++ src/Estat.java
@@ -0,0 +1,7 @@
+package  IA.freezing-wookie
+
+import java.util.ArrayList
+
+class Estat {
+	private ArrayList<int> 
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package  IA.freezing-wookie
+
+class Main {
+	public static void main(String[] args) {
+		
+	}
+}
