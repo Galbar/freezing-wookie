4e39fa0a78b98df2ead6089b657abc1222d6e780
Daniel Otero Avalle <daniel.oteroavalle@gmail.com>
2014-10-10 14:51:15 +0200

    master update

--- src/freezingwookie/ControladorEstado.java
+++ src/freezingwookie/ControladorEstado.java
@@ -1,5 +1,9 @@
 package freezingwookie;
 
+import IA.Desastres.*;
+import java.io.Console;
+import java.lang.Integer;
+
 public class ControladorEstado {
 
 	/**
@@ -7,7 +11,18 @@ public class ControladorEstado {
 	*/
 	public static void main(String[] args) {
 
-		Estado e = new Estado();
+		Console console = System.console();
+
+		int nc = Integer.parseInt(console.readLine("Introduce el nº centros:"));
+		int nh = Integer.parseInt(console.readLine("Introduce el nº helicopteros:"));
+		int seed = Integer.parseInt(console.readLine("Introduce el random seed:"));
+		Centros cs = new Centros(nc,nh,seed);
+
+		nh = Integer.parseInt(console.readLine("Introduce el nº grupos:"));
+		seed = Integer.parseInt(console.readLine("Introduce el random seed:"));
+		Grupos gs = new Grupos(nh,seed);
+
+		Estado e = new Estado(cs,gs);
 
 		e.primeraSol();
 
